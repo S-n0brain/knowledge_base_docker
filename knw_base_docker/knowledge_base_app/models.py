f45b7cd6e6b4f299fa238242e74dc941edc3eff6
Sergey <koltafvhdgv@gmail.com>
2025-09-15 06:35:00 +0900

    Создал модель для раг, (разбиение на чанки текста, сохранение эмбэдингов при сохранении и после) ДО ПРОВЕДЕНИЯ МИГРАЦИЙ

--- knw_base_docker/knowledge_base_app/models.py
+++ knw_base_docker/knowledge_base_app/models.py
@@ -6,11 +6,16 @@ from django.utils.html import strip_tags
 from pgvector.django import VectorExtension
 from pgvector.django import VectorField
 
+from openai import OpenAI
+from knowledge_base.settings import API_GPT_KEY
+
 class PublishedArticleManager(models.Manager):
     def get_queryset(self) -> models.QuerySet:
         return super().get_queryset().filter(published=True).select_related('topic')
 
 class Article(models.Model):
+    BASE_URL_MODAL = "https://api.proxyapi.ru/openai/v1"
+
     title = models.CharField(max_length=250, verbose_name="Название статьи")
     slug = models.SlugField(max_length=200, unique=True, verbose_name="URL-адрес статьи")
     topic = models.ForeignKey(to="Topic", on_delete=models.CASCADE, related_name="articles", verbose_name="Тема статьи")
@@ -31,7 +36,46 @@ class Article(models.Model):
 
     def get_plain_content(self):
         return strip_tags(self.content)
+    
+    @staticmethod
+    def chunk_text(text: str, chunk_size=500):
+        # Разделение текста на фрагменты заданного размера
+        words = text.split()
+        for i in range(0, len(words), chunk_size):
+            yield ' '.join(words[i:i + chunk_size])
 
+    def create_article_chunks(self):
+        self.chunks.all().delete()  # type: ignore
+        plain_content = self.get_plain_content()
+        try:
+            client = OpenAI(api_key=API_GPT_KEY, base_url=self.BASE_URL_MODAL)
+        except Exception:
+            embedding = None
+            ArticleChunk.objects.create(article=self, text=plain_content, embedding=embedding)
+        else:
+            for chunk in self.chunk_text(plain_content):
+                try:
+                    response = client.embeddings.create(
+                        input=chunk,
+                        model="text-embedding-3-small"
+                    )
+                    embedding = response.data[0].embedding
+                except Exception as e:
+                    embedding = None
+                    print(f"Ошибка при создании эмбеддинга: {e}")
+                ArticleChunk.objects.create(article=self, text=chunk, embedding=embedding)
+
+    def save(self, *args, **kwargs):
+        # Сохранение модели с генерацией векторного представления
+        if self.pk:
+            try:
+                old_content = Article.objects.get(pk=self.pk).content
+            except Article.DoesNotExist:
+                old_content = None
+        super().save(*args, **kwargs)
+        if not self.pk or (self.content != old_content):
+            self.create_article_chunks()
+            
     class Meta:
         ordering = ['title']
         verbose_name = "Статья"
@@ -87,4 +131,5 @@ class Migration(migrations.Migration):
 
 class ArticleChunk(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="chunks", verbose_name="Статья")
-    embedding = VectorField(dimensions=1536, null=True, verbose_name="Векторное представление")
+    embedding = VectorField(dimensions=1536, null=True, verbose_name="Векторное представление", default=None)
+    text = models.TextField(blank=True, verbose_name="Текстовый фрагмент")
